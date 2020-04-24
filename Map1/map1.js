7a8580148a33a1aad2bbca0a47c82a18f4e3790f
User1 <user@example.com>
2020-04-24 18:27:51 -0500

    commit

--- Map1/map1.js
+++ Map1/map1.js
@@ -1,6 +1,6 @@
-var map1 = L.map('mymap').setView([29.969552, -90.109885], 4
+var map1 = L.map('map1').setView([29.969552, -90.109885], 4
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 19, attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'});
-var Audubon Park = L.marker([29.934360, -90.123689]).addTo(map1)
+var Audubon Park = L.marker([29.934360, -90.123689]).addTo(Map1)
 var Four Corners Of Audubon Park = L.polygon([
   [29.936117, -90.125480],
   [29.933283, -90.121480],
---
+++ Map3/map3.js
@@ -0,0 +1,32 @@
+var Map3 = L.map('map3').setView([29.969552, -90.109885], 4)
+var statesLayer = L.layerGroup().addTo(Map3)
+var grayBasemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}').addTo(Map3)
+var streetsBasemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}').addTo(Map3)
+var demographics = 'https://geog4046.github.io/assignment-resources/data/us_state_demographics_ESRI_2010A.geojson'
+jQuery.getJSON(demographics, function (data) {
+  L.geoJSON(data, {
+    style: states,
+    onEachFeature: populationResults
+  }).addTo(Map3)
+var states = function (feature) {
+  var statePopulation = feature.properties.POPULATION
+  var stateColor = 'olive'
+  if (statePopulation > 1000000) { stateColor = 'green' }
+  return {
+    color: stateColor,
+    weight: 2,
+    fillOpacity: 0.5
+  }
+var populationResults = function (feature, layer) {
+  var stateName = feature.properties.STATE_NAME
+  var statePop = feature.properties.POPULATION
+  layer.bindPopup('Population of ' + stateName + ' is ' + statePop + '<br>Less Than: 1000000')
+  statesLayer.addLayer(layer)
+}
+var basemaps = {
+  'Streets': streetsBasemap,
+}
+var layers = {
+  'Area of each state': statesLayer
+}
+L.control.layers(basemaps, layers).addto(Map3)
